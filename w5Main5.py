c65bde3a1eb4ddbdc4db79f9d020450f637ed847
KimBoKyu <sa5469@naver.com>
2016-03-30 13:17:44 +0900

    Added files via upload

---
+++ w5Main5.py
@@ -0,0 +1,14 @@
+﻿marksTmp = raw_input('성적을 입력하세요 (0~100)')
+marks = float(marksTmp)
+print "입력 값은",marks
+
+if(90<=marks<=100):
+    print "My grade is %s" % "A"
+elif(80<=marks<90):
+    print "My grade is %s" % "B"
+elif(70<=marks<80):
+    print "My grade is %s" % "C"
+elif(60<=marks<70):
+    print "My grade is %s" % "D"
+else:
+    print "My grade is %s" % "F"
