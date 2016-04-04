caf556f7b7d07d0fab425d38913bbe0bcedd61b7
201611059 김보규 <sa5469@naver.com>
2016-04-04 17:47:19 +0900

    BMI 지수 계산기

---
+++ w5mian8.py
@@ -0,0 +1,17 @@
+﻿height = input("Put in your height : ")
+weight = input("Put in your weight : ")
+BMI =  weight * 10000 / (height * height)
+print BMI
+
+if BMI < 18.5 :
+    print "low weight"
+elif BMI >= 18.5 and BMI < 25 :
+    print "nomal weight"
+elif BMI >= 25 and BMI < 30 :
+    print "overweight"
+elif BMI >= 30 and BMI < 35 :
+    print "little overweight"
+elif BMI > 35 :
+    print "super overweight"
+else :
+    print "Input Error"
