9bf3062c09a0acc51c78764af979c862293c9c55
201611059 김보규 <sa5469@naver.com>
2016-04-11 17:36:16 +0900

    (1<=x<=1000)일때 4의 배수이면서 5의 배수는 아닌 수의 합 구하기

---
+++ w6Mian12.py
@@ -0,0 +1,25 @@
+﻿"""
+@author KBK
+@since 160411
+"""
+def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if (i%4== 0 and i%5 != 0):
+            x.append(i)
+    sum = 0
+    for b in range(0,len(x)):
+        sum = sum+x[b]
+    return sum
+    
+def lab6():
+    aList=1001
+    labsum=sumList(aList)
+    print labsum
+
+def main():
+    lab6()
+
+if __name__=="__main__":
+        main()
+wn = raw_input("If you want to exit program, Press Enter!")
