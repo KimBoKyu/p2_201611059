63d09af93015918029faa5fe8b99645698741269
201611059 김보규 <sa5469@naver.com>
2016-05-03 22:00:36 +0900

    9주차 과제

--- w9main.py
+++ w9main.py
@@ -1,9 +1,9 @@
 ﻿import matplotlib
 import matplotlib.pyplot as plt
 
-def charCount(word):
+def charCount(word1):
     d=dict()
-    for c in word:
+    for c in word1:
         if c not in d:
             d[c]=1
         else:
@@ -13,10 +13,23 @@ def charCount(word):
     plt.xticks(range(0,len(d)),list(d.keys()))
     plt.show()
 
+def countDigitsLetters(word2):
+    d={"number":0, "word":0}
+    for c in word2:
+        if c.isdigit()==True:
+            d["number"]=d["number"]+1
+        elif c.isdigit()==False:
+            d["word"]=d["word"]+1
+    print d
+    plt.bar(range(0,len(d)),d.values(), align='center')
+    plt.xticks(range(0,len(d)),list(d.keys()))
+    plt.show()
 
 def lab9():
-    word = 'sangmyung university'
+    word1 = "sangmyung university"
     charCount(word)
+    word2 = "7 hongjidong"
+    countDigitsLetters(word2)
 
 def main():
     lab9()
