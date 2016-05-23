3db7dd0c2b6f7a8e13a53b61d06b76f84ee2d79b
201611059 김보규 <sa5469@naver.com>
2016-05-23 17:26:10 +0900

    12주차 과제 python 들어있는 문장 프린트, line을 LINE으로

---
+++ w12main.py
@@ -0,0 +1,47 @@
+﻿import os
+mydir=os.getcwd()
+
+def homework_one():
+    filename=raw_input("Input your filename(ex:python.txt):")
+    myfilename=os.path.join(mydir,filename)
+    try:
+        myfile=open(myfilename, 'r')
+        contents=myfile.readlines()
+        for i in range(0,len(contents)):
+            if contents[i].find('Python') >= 0:
+                print contents[i]
+        myfile.close()
+    except IOError as e:
+        print e
+
+def homework_two():
+    filetwo=open('output.txt', 'w')
+    line1='first line\n'
+    filetwo.write(line1)
+    line2='second line\n'
+    filetwo.write(line2)
+    line3='third line'
+    filetwo.write(line3)
+    filetwo.close()
+    myfiletwo=open('output.txt', 'r')
+    contentstwo=myfiletwo.readlines()
+    for a in range(0,len(contentstwo)):
+        if contentstwo[a].find('l') >= 0:
+            result = contentstwo[a].split()
+        for b in range(0,len(result)):
+            if result[b].find('l') >= 0:
+                print result[b].upper()
+    myfiletwo.close()
+    
+def lab12():
+    print "Homework one"
+    homework_one()
+    print "Homework two"
+    homework_two()
+
+def main():
+    lab12()
+    raw_input()
+
+if __name__=="__main__":
+    main()
