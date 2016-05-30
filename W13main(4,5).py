b51db3c0e5526f83c75f3ea65947ac8a9341ba84
201611059 김보규 <sa5469@naver.com>
2016-05-31 00:43:23 +0900

    5월30일 phthon.txt에 number 추가하기 / 좌표 2개를 이용하여 사각형그리기

--- W13main(4,5).py
+++ W13main(4,5).py
@@ -1,4 +1,4 @@
-﻿import turtle
+import turtle
 t1=turtle.Turtle()
 wn=turtle.Screen()
 def homework():
@@ -13,8 +13,6 @@ def homework():
         fin2.close()
     except Exception as e:
         print e
-homework()
-aFile=raw_input("Input filename ex)recoords.txt: ")
 def getCoordsFromFile(aFile):
     filehome=open(aFile)
     coords=list()
@@ -23,7 +21,6 @@ def getCoordsFromFile(aFile):
         coords.append([(line1[0],line1[1]), (line1[2],line1[3].strip())])
     filehome.close()
     return coords
-coords=getCoordsFromFile(aFile)
 def drawSquareWithCoords(coords):
     for coord in coords:
         x1=int(coord[0][0])
@@ -31,9 +28,22 @@ def drawSquareWithCoords(coords):
         y1=int(coord[0][1])
         y2=int(coord[1][1])
         print x1,y1,x2,y2
+        t1.penup()
         t1.goto(x1,y1)
+        t1.pendown()
         for i in range(0,4):
             t1.fd(x1-x2)
             t1.rt(90)
-drawSquareWithCoords(coords)
-wn.exitonclick()
+def lab13():
+    homework()
+    aFile=raw_input("Input filename ex)recoords.txt: ")
+    getCoordsFromFile(aFile)
+    coords=getCoordsFromFile(aFile)
+    drawSquareWithCoords(coords)
+def main():
+    lab13()
+    wn.exitonclick()
+
+if __name__=="__main__":
+        main()
+    
